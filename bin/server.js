fa9bdb32b3e800b307ad75ada4591a64af984832
Chipzstar <chipzstar.dev@googlemail.com>
2021-07-28 15:30:00 +0100

    v26

--- bin/server.js
+++ bin/server.js
@@ -11,8 +11,7 @@ const wss = new WebSocket.Server({ noServer: true })
 const setupWSConnection = require('./utils.js').setupWSConnection
 
 const host = process.env.HOST || 'localhost'
-console.log("PORT:", process.env.PORT)
-const port = Number(process.env.PORT) || 1234
+const port = process.env.PORT || 1234
 
 const server = https.createServer({
   key: fs.readFileSync(path.join(__dirname, 'cert', 'private_key.pem')),
@@ -35,4 +34,6 @@ server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, handleAuth)
 })
 
-server.listen(port, host, () => console.log(`running at '${host}' on port ${port}`))
+server.listen({ host, port })
+
+console.log(`running at '${host}' on port ${port}`)
